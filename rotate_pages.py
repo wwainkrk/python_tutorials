fcc32bd8f954dc32bb05cbac5951e389c5deb916
Sebastian Warszawa <warsaw0902@gmail.com>
2019-05-11 13:22:14 +0200

    Change function in rotate file, different direction to rotate second page; All functionality for merge two pdf files;

--- rotate_pages.py
+++ rotate_pages.py
@@ -7,7 +7,7 @@ def rotate_pages(path_pdf):
 
     page_1 = pdf_reader.getPage(0).rotateClockwise(90)  # obrot strony o 90 stopni
     pdf_writer.addPage(page_1)                          # dodanie strony do tworzonego obiektu pdf
-    page_2 = pdf_reader.getPage(1).rotateClockwise(90)
+    page_2 = pdf_reader.getPage(1).rotateCounterClockwise(90)
     pdf_writer.addPage(page_2)
 
     pdf_writer.addPage(pdf_reader.getPage(2))           # dodanie nieobroconej strony do obiektu
