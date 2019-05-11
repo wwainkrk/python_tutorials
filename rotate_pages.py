400ac8ab4b724746e146faf01659af65c33a5b9b
Sebastian Warszawa <warsaw0902@gmail.com>
2019-05-11 13:43:44 +0200

    Update .gitingore - add .pdf files, execute rotate function, add writing to file from PDF object

--- rotate_pages.py
+++ rotate_pages.py
@@ -1,9 +1,21 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def rotate_pages():
-    pdf_writer = PdfFileWriter()
-    pdf_reader = PdfFileReader()
+
+def rotate_pages(path_pdf):
+    pdf_writer = PdfFileWriter()                    # obiekt klasy zapisujacej
+    pdf_reader = PdfFileReader(path_pdf)            # obiekt klasy czytajacej
+
+    page_1 = pdf_reader.getPage(0).rotateClockwise(90)  # obrot strony o 90 stopni
+    pdf_writer.addPage(page_1)                          # dodanie strony do tworzonego obiektu pdf
+    page_2 = pdf_reader.getPage(1).rotateClockwise(90)
+    pdf_writer.addPage(page_2)
+
+    pdf_writer.addPage(pdf_reader.getPage(2))           # dodanie nieobroconej strony do obiektu
+
+    with open('rotate_pages.pdf', 'wb') as f:
+        pdf_writer.write(f)
 
 
 if __name__ == "__main__":
-    rotate_pages()
+    path = 'reportlab-userguide.pdf'
+    rotate_pages(path)
