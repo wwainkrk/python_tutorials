ddaa1aef1c36d383bf9ca8fc225b8b78e6735084
Sebastian Warszawa <warsaw0902@gmail.com>
2020-04-07 19:11:08 +0200

    Function for division list and minimize range in search mechanism

--- BinarySearch_virtEnv/BinarySearch.py
+++ BinarySearch_virtEnv/BinarySearch.py
@@ -24,3 +24,22 @@ def find_index(elements, value):
         return index
 """
 
+"""
+Most important condition: List has to be SORTED before function execution
+"""
+
+
+def find_index(elements, value):
+    left, right = 0, len(elements) - 1
+    middle = (left + right) // 2                                                # integer division
+
+    if elements(middle) == value:                                               # conditions for minimize range
+        return middle
+    elif elements(middle) < value:
+        left = middle + 1
+    elif elements(middle) > value:
+        right = middle - 1
+
+
+
+
