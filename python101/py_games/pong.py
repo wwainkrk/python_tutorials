4af82ccd2e86e12ca638bd8f367c1a8566558150
Sebastian Warszawa <warsaw0902@gmail.com>
2020-04-18 23:51:19 +0200

    Simply class for handling a chart for game, constructor with resolution of chart for game and drawing function for list of objects

---
+++ python101/py_games/pong.py
@@ -0,0 +1,38 @@
+import pygame
+import pygame.locals
+import time
+
+
+class Board(object):
+    """
+    Chart for game. Is responsible for drawing game window.
+    """
+    def __init__(self, width, height):
+        """
+        Constructor method. Preparing setting for game window.
+        :param width:
+        :param height:  resolution of window
+        """
+        self.surface = pygame.display.set_mode((width, height), 0, 32)
+        pygame.display.set_caption('Simple Pong')
+
+    def draw(self, *args):
+        """
+        This drawing game window
+
+        :param args: list of objects to draw
+        """
+        background = (230, 255, 255)
+        self.surface.fill(background)
+
+        for drawable in args:
+            drawable.draw_on(self.surface)
+
+            # Only here the actual drawing takes place
+            # Before, we only decided what and how to draw
+            pygame.display.update()
+
+
+board = Board(600, 400)
+board.draw()
+time.sleep(5)
