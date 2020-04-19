acd80156300cc7e0e4a531aeaaaa7c64cea930b7
Sebastian Warszawa <warsaw0902@gmail.com>
2020-04-19 21:33:22 +0200

    Added Ball class which inherited from parent class, we overwrite constructor from parent class and add some methods to controll the ball in game

--- python101/py_games/drawable.py
+++ python101/py_games/drawable.py
@@ -19,3 +19,39 @@ class Drawable(object):
 
 
 class Ball(Drawable):
+    """
+    Ball, she controls speed and direction itself
+    """
+    def __init__(self, width, height, x, y, color=(255, 0, 0), x_speed=3, y_speed=3):
+        super(Ball, self).__init__(width, height, x, y, color)
+        pygame.draw.ellipse(self.surface, self.color, [0, 0, self.width, self.height])  # Draw round shape on surface
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.start_x = x
+        self.start_y = y
+
+    def bounce_y(self):
+        """
+        Change direction on axis y to opposite
+        """
+        self.y_speed *= -1
+
+    def bounce_x(self):
+        """
+        Change direction on axis x to opposite
+        """
+        self.x_speed *= -1
+
+    def move(self):
+        """
+        Move a ball by speed vector
+        """
+        self.rect.x += self.x_speed
+        self.rect.y += self.y_speed
+
+    def reset(self):
+        """
+        Reset position of the ball to start position and change direction on y axis
+        """
+        self.rect.move(self.start_x, self.start_y)
+        self.bounce_y()
