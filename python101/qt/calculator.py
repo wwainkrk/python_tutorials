5be12f4d7022d2df3ab569e85d1091badc967e97
Sebastian Warszawa <warsaw0902@gmail.com>
2020-04-28 00:47:42 +0200

    New settings in interface, labels, vertical/table grid layout, icon in main window

--- python101/qt/calculator.py
+++ python101/qt/calculator.py
@@ -1,15 +1,32 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout
+from PyQt5.QtGui import QIcon
 import sys
+# from __future__ import unicode_literals             # Polish characters
 
 
 class Calculator(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.interface()
+        self.interface()                        # methods for window displaying
 
     def interface(self):
-        self.resize(400, 200)
+        # labels
+        label1 = QLabel("Number 1:", self)
+        label2 = QLabel("Number 2:", self)
+        label3 = QLabel("Result:", self)
+
+        # assign labels to grid layout
+        layout_tab = QGridLayout()
+        layout_tab.addWidget(label1, 0, 0)
+        layout_tab.addWidget(label2, 0, 1)
+        layout_tab.addWidget(label3, 0, 2)
+
+        # assign grid layout to window
+        self.setLayout(layout_tab)
+
+        self.setGeometry(20, 20, 400, 200)
+        self.setWindowIcon(QIcon("calculator.png"))
         self.setWindowTitle("Simply calculator")
         self.show()
 
