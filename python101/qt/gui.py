59069720c6c6399fb0f2bd554e61c1935702fa75
Sebastian Warszawa <warsaw0902@gmail.com>
2020-05-13 12:56:26 +0200

    separated file for shapes, new methods for Shape objects, handling others shapes to draw

--- python101/qt/gui.py
+++ python101/qt/gui.py
@@ -1,6 +1,6 @@
-from enum import Enum
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import QRect
+# from PyQt5.QtCore import QRect
+from PyQt5.QtWidgets import QHBoxLayout
+from shapes import Shape, Shapes
 
 
 class UIWidget(object):
@@ -11,40 +11,14 @@ class UIWidget(object):
 
         :param Widget: UIWidget object for execute in main class
         """
-        self.shape = Shapes.Ellipse             # hardcoded shape to draw
-        self.rect = QRect(1, 1, 101, 101)
+        self.shape = Shape(self, Shapes.Polygon)            # hardcoded shape to draw
 
-        self.colorP = QColor(0, 0, 0)
-        self.colorB = QColor(200, 30, 40)
+        self.resize(150, 150)
+        print("setup_ui")
+        systemh = QHBoxLayout()
+        systemh.addWidget(self.shape)
 
-        #self.resize(150, 150)
-        #print("setup_ui")
+        self.setLayout(systemh)
         self.setWindowTitle("Widgets App")
 
-    def paintEvent(self, e):
-        qp = QPainter()
-        qp.begin(self)
-        #print("paintEvent")
-        self.draw_figures(e, qp)                # function to build shape in Painter
-        qp.end()
 
-    def draw_figures(self, e, qp):
-        # print("drawFigures")
-
-        #print("draw_figures")
-        qp.setPen(self.colorP)
-        qp.setBrush(self.colorB)
-        qp.setRenderHint(QPainter.Antialiasing)
-
-        if self.shape == Shapes.Rect:
-            qp.drawRect(self.rect)              # the same argument as in drawEllipse, for surface
-        elif self.shape == Shapes.Ellipse:
-            qp.drawEllipse(self.rect)
-
-
-class Shapes(Enum):
-    # Rect, Ellipse, Polygon, Line = range(4)    # similar way to simulate enum type
-    Rect = 0
-    Ellipse = 1
-    Polygon = 2
-    Line = 3
---
+++ python101/qt/shapes.py
@@ -0,0 +1,69 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPainter, QColor, QPolygon
+from PyQt5.QtCore import QPoint, QRect, QSize
+from enum import Enum
+
+
+class Shapes(Enum):
+    # Rect, Ellipse, Polygon, Line = range(4)    # similar way to simulate enum type
+    Rect = 0
+    Ellipse = 1
+    Polygon = 2
+    Line = 3
+
+
+class Shape(QWidget):
+    """Class where we can define which widget we want to draw"""
+    rect = QRect(1, 1, 101, 101)
+    points = QPolygon([
+            QPoint(1, 101),  # begin point (x, y)
+            QPoint(51, 1),
+            QPoint(101, 101)
+    ])
+
+    def __init__(self, parent, shape=Shapes.Rect):
+        super(Shape, self).__init__(parent)
+        print("init Shape")
+        self.shape = shape                          # shape which will be draw
+
+        # colors of pen and which will be inside surface
+        self.colorP = QColor(0, 0, 0)
+        self.colorB = QColor(255, 255, 255)
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        print("paintEvent")
+        self.draw_figures(e, qp)                # function to build shape in Painter
+        qp.end()
+
+    def draw_figures(self, e, qp):
+        print("draw_figures")
+        qp.setPen(self.colorP)
+        qp.setBrush(self.colorB)
+        qp.setRenderHint(QPainter.Antialiasing) # more 'smoother' shapes
+
+        if self.shape == Shapes.Rect:
+            qp.drawRect(self.rect)              # the same argument as in drawEllipse, for surface
+        elif self.shape == Shapes.Ellipse:
+            qp.drawEllipse(self.rect)
+        elif self.shape == Shapes.Polygon:
+            qp.drawPolygon(self.points)
+        elif self.shape == Shapes.Line:
+            qp.drawLine(self.rect.topLeft(), self.rect.bottomRight())
+        else:
+            qp.drawRect(self.rect)
+
+    def size_hint(self):
+        return QSize(150, 150)
+
+    def min_size_hint(self):
+        return QSize(102, 102)
+
+    def set_shape(self, shape):
+        self.shape = shape
+        self.update()
+
+    def set_colorB(self, r=0, g=0, b=0):
+        self.colorB = QColor(r, g, b)       # arguments provided as RGB
+        self.update()
