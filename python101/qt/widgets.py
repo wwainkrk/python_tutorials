3a178c57b3c3107d4e4235aa6302a734288899af
Sebastian Warszawa <warsaw0902@gmail.com>
2020-05-15 19:47:52 +0200

    Two variables in constructor, signal/slots handling for changes na this values, execute a setting color function

--- python101/qt/widgets.py
+++ python101/qt/widgets.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals             # Polish characters
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QColor
 from gui import UIWidget
 
 
@@ -13,6 +14,41 @@ class Widgets(QWidget, UIWidget):
         # Signals & slots
         self.group_checkbtn.buttonClicked[int].connect(self.set_shape)
         self.shape_check.clicked.connect(self.activate_shape)
+
+        # Channel and color variables
+        self.channel = {'R'}
+        self.colorI = QColor(0, 0, 0)
+
+        # Signal/slots mechanism for changing values in LCD and RadioButton group
+        for i in range(self.radio_layout.count()):
+            self.radio_layout.itemAt(i).widget().toggled.connect(self.set_channel_radiobtn) # toggled = changed
+        self.slider.valueChanged.connect(self.change_color)                                  # slider changes
+
+    def set_channel_radiobtn(self, value):
+        """
+        Reset channels with color and put selected one
+        """
+        self.channel = set()
+        if value:
+            self.channel.add(self.sender().text())
+
+    def change_color(self, value):
+        """
+        Displaying current value on lcd, and take value from slider to setting color method
+        """
+        self.lcd.display(value)
+        if 'R' in self.channel:
+            self.colorI.setRed(value)
+        elif 'G' in self.channel:
+            self.colorI.setGreen(value)
+        elif 'B' in self.channel:
+            self.colorI.setBlue(value)
+
+        self.active_shape.set_colorI(
+            self.colorI.red(),
+            self.colorI.green(),
+            self.colorI.blue()
+        )
 
     def set_shape(self, value):
         # print("set_shape")
