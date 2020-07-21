567780949f04a66368aee00dbb71c335df253594
Sebastian Warszawa <warsaw0902@gmail.com>
2020-07-21 17:29:43 +0200

    Changes for post panel with new headers, columns, filters etc

--- django_book/mysite/blog/admin.py
+++ django_book/mysite/blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')     # columns/headers
+    list_filter = ('status', 'created', 'publish', 'author')            # filtr section
+    search_fields = ('title', 'body')                                   # search bar
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
