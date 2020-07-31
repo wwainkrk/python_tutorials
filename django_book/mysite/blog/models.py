cc75fde5df8943c4a8be7f1e2273a7a4962e3214
Sebastian Warszawa <warsaw0902@gmail.com>
2020-07-31 16:02:20 +0200

    reverse() from urlresolvers changed into urls module, CSS file for blog application - downloaded from GitHub

--- django_book/mysite/blog/models.py
+++ django_book/mysite/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
