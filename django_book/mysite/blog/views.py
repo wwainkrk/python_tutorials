bcf21eb2ab42c04af91201028d53087f7f4a63ff
Sebastian Warszawa <warsaw0902@gmail.com>
2020-08-03 15:02:50 +0200

    Generic view for post list, new class in views with different url method

--- django_book/mysite/blog/views.py
+++ django_book/mysite/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# Create your views here.
+# Create your views here.'
 
 
 def post_list(request):
-    object_list = Post.published.all()
+    object_list = Post.objects.all()
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -35,3 +36,11 @@ def post_detail(request, day, month, year, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+# Generic view instead simply post view
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
