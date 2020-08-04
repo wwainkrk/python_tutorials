004af317f13bd96b8a139fde230d998d38b392b0
Sebastian Warszawa <warsaw0902@gmail.com>
2020-08-04 16:21:51 +0200

    New model and migrations for Comments table

--- django_book/mysite/blog/models.py
+++ django_book/mysite/blog/models.py
@@ -44,3 +44,21 @@ class Post(models.Model):
                              self.publish.strftime('%m'),
                              self.publish.strftime('%d'),
                              self.slug])
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return "Comment added by {} for post {}".format(self.name, self.post)
