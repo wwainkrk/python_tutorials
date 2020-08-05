90ff535b4ce8dcef64a94ef35e2df2abf36ad734
Sebastian Warszawa <warsaw0902@gmail.com>
2020-08-05 17:59:12 +0200

    New Installed Apps for our project, tag manager, tag field added in model, migration with new field

--- django_book/mysite/blog/models.py
+++ django_book/mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -19,6 +20,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
+    tags = TaggableManager()
 
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
