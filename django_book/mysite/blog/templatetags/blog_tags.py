8057cf13ea66676e6aef5a081b3dd456714c9bf0
Sebastian Warszawa <warsaw0902@gmail.com>
2020-08-12 15:23:14 +0200

    Markdown in venv, markdown filter, changes in templates with markdown filter

--- django_book/mysite/blog/templatetags/blog_tags.py
+++ django_book/mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from ..models import Post
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 
 register = template.Library()
 
@@ -19,3 +22,8 @@ def get_most_commented_posts(count=5):
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
