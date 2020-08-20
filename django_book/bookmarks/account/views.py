ba9d55cb0118beb8808ce97d9655e55c3b089ca1
Sebastian Warszawa <warsaw0902@gmail.com>
2020-08-20 13:16:48 +0200

    LoginForm for simple login mechanism, new view for authenthication with HttpResponse

--- django_book/bookmarks/account/views.py
+++ django_book/bookmarks/account/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm()
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Authentication successfully')
+            else:
+                return HttpResponse('Account is blocked')
+        else:
+            return HttpResponse('Wrong authentication data')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {
+                  'form' : form
+    })
