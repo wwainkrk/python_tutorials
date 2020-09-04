454a3832a7bfbf1dccd4abf0f9fb0be7006bba6d
Sebastian Warszawa <warsaw0902@gmail.com>
2020-09-04 17:58:56 +0200

    New ModelForm for User Registration based on Django User table

--- django_book/bookmarks/account/forms.py
+++ django_book/bookmarks/account/forms.py
@@ -1,6 +1,22 @@
 from django import forms
-
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'last_name', 'email')
+
+    def clean_password(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords are not the same')
+
+        return cd['password2']
