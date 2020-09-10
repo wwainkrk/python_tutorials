8ad96e46a8c182a164fc7293b466e53ec01a4c6c
Sebastian Warszawa <warsaw0902@gmail.com>
2020-09-10 17:35:00 +0200

    Templates for registration, labels for passwords in modelform for User

--- django_book/bookmarks/account/forms.py
+++ django_book/bookmarks/account/forms.py
@@ -7,14 +7,14 @@ class LoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ('username', 'last_name', 'email')
 
-    def clean_password(self):
+    def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords are not the same')
