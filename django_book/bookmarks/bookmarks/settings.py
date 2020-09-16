1a9743dba889bb8e2468c4e91e106fca8d8e3106
Sebastian Warszawa <warsaw0902@gmail.com>
2020-09-16 11:21:15 +0200

    New application in bookmarks project, first Model with migration, additional method for model

--- django_book/bookmarks/bookmarks/settings.py
+++ django_book/bookmarks/bookmarks/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['my-website.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
